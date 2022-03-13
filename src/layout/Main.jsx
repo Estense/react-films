93f2c9c3df78b3e4682a6d866eb3cc699a4068c1
Estense <a.vernov16@gmail.com>
2022-03-13 18:04:00 +0500

    deploy

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,21 +12,28 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=Matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=Matrix`)
       .then((response) => response.json())
       .then((data) => this.setState({ films: data.Search, loading: false }))
-      .catch();
+      .catch((err) => {
+        console.error(err);
+        this.setState({loading: false})
+      });
   }
 
   searchMovie = (movieTitle, type) => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${movieTitle}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieTitle}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ films: data.Search, loading: false }));
+      .then((data) => this.setState({ films: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({loading: false})
+      })
   };
 
   render() {
